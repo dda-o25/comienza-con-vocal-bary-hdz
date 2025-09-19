3a46dd084377d37a960ebc3ab946d95c75cea985
Baryhdz <bruce.hernandez@iteso.mx>
2025-09-18 21:56:59 -0600

    prueba1_vocales

--- comienza_con_vocal.py
+++ comienza_con_vocal.py
@@ -3,13 +3,20 @@ Inserta el encabezado aquí y escribe tu código abajo
 """
 
 # Declaraciones
-CONSTANTE = valor
+vocales = "aeiouáéíóúAEIOUÁÉÍÓÚ"
 
 # Entradas
-entrada = input()
+text = input("Ingresa una palabra: ")
 
 # Proceso
-
+texto = text.strip()  # Elimina espacios en blanco al inicio y al final
+if texto== "":
+    print("No ingresaste ninguna palabra")
+else:
+    if texto[0] in vocales:
+        salida ="La palabra comienza con una vocal"
+    else:
+        salida ="La palabra no comienza con una vocal"
 
 # Salidas
 print(salida)
